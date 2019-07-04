ef020dfe9b046cff52f7b2bf83af0088273dd0fd
Hao Wang <ge_nah@hotmail.com>
2019-07-04 10:54:01 -0700

    Done.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,16 @@
 class Destination < ApplicationRecord
+  has_many :posts
+  has_many :bloggers, through: :posts
 
+  def most_recent_posts
+    self.posts.sort_by{|post| post.created_at}[0..4]
+  end
+
+  def most_liked_post
+    self.posts.sort_by{|post| post.likes}.last
+  end
+
+  def average_age
+    self.bloggers.map{|blogger| blogger.age}.sum / self.bloggers.count
+  end
 end
